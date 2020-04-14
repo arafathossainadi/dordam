c25964266624c85e89e0536ecdd388feaf519710
arafathossainadi <muanakaa@gmail.com>
2020-04-14 13:52:35 +0600

    added mobile hamburger menu

--- js/sticky-nav.js
+++ js/sticky-nav.js
@@ -1,10 +1,26 @@
 window.onscroll = function() {myFunction()};
 var header = document.getElementById("myHeader");
+var mobileheader = document.getElementById("mobileHeader");
+var mobilenav = document.getElementById("navBar");
 var sticky = header.offsetTop;
+var sticky2 = mobileheader.offsetTop;
+var sticky3 = mobilenav.offsetTop;
 function myFunction() {
 	if (window.pageYOffset > sticky) {
 		header.classList.add("sticky");
 	} else {
 		header.classList.remove("sticky");
+	}
+
+	if (window.pageYOffset > sticky2) {
+		mobileheader.classList.add("mobile-sticky");
+	} else {
+		mobileheader.classList.remove("mobile-sticky");
+	}
+
+	if (window.pageYOffset > sticky3) {
+		mobilenav.classList.add("mobile-sticky");
+	} else {
+		mobilenav.classList.remove("mobile-sticky");
 	}
 }
